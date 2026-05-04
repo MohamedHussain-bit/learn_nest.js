534eecd9669c667517fd73efa5843380514901e4
MohamedHussain-bit <godzela.1242006@gmail.com>
2026-05-04 23:54:07 +0300

    add method to create product on controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,14 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+
+type ProductType = {
+  id: number;
+  name: string;
+  price: number;
+};
 
 @Controller()
 export class ProductController {
+  private products: ProductType[] = [
+    { id: 1, name: 'Product 1', price: 10.99 },
+    { id: 2, name: 'Product 2', price: 19.99 },
+    { id: 3, name: 'Product 3', price: 5.99 },
+  ];
+  // GET  http://localhost:5000/api/v1/products
+  @Post('/api/v1/products')
+  public createProduct(@Body() body: any) {
+    console.log(body);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return body;
+  }
   // GET  http://localhost:5000/api/v1/products
   @Get('/api/v1/products')
   public getAllProducts() {
-    return [
-      { id: 1, name: 'Product 1', price: 10.99 },
-      { id: 2, name: 'Product 2', price: 19.99 },
-      { id: 3, name: 'Product 3', price: 5.99 },
-    ];
+    return this.products;
   }
 }
